39f4ee3eb0861283b16f99b9065dc0ec51a60e8c
Ramon <admin@mail.ru>
2023-06-19 14:17:15 +0300

    19.06.2023

--- frontend/src/pages/addItem/addItem.tsx
+++ frontend/src/pages/addItem/addItem.tsx
@@ -5,36 +5,82 @@ import { useAddItem } from './useAddItem'
 
 const AddItem: React.FC = () => {
 
-const {categories} = useAddItem()
+const {categories, uploadImage, register, errors, handleSubmit, onSubmit} = useAddItem()
 
 return (
   <div className={style.root}>
+    <form onSubmit={handleSubmit(onSubmit)}>
     <h2>Добавление товара</h2>
+
     <div>
       <label>Название:</label>
-      <input type='text' />
+      <input 
+        {...register('name', {
+          required: 'Обязательное поле!'
+        })}
+        type='text' 
+        name='name'
+        placeholder='Введите название'
+      />
     </div>
+
+    {errors.name && <div style={{color: "red"}}>{errors.name.message}</div>}
+
     <div>
       <label>Категория:</label>
-      <select>
+      <select
+        {...register('category')}
+        name='category'
+      >
         {categories && categories.map(cat => (
           <option>{cat.name}</option>
         ))}
       </select>
     </div>
+
     <div>
       <label>Цена:</label>
-      <input type='text' />
+      <input 
+        {...register('price', {
+          required: 'Обязательное поле!'
+        })}
+        type='text'
+        name='price'
+        placeholder='Введине цену' 
+      />
     </div>
+
+    {errors.price && <div style={{color: "red"}}>{errors.price.message}</div>}
+
     <div>
       <label>Скидка:</label>
-      <input type='text' />
+      <input 
+        {...register('sale', {
+          required: 'Обязательное поле!'
+        })}
+        type='text' 
+        name='sale'
+        placeholder='Укажите скидку в %'
+      />
     </div>
+
+    {errors.sale && <div style={{color: "red"}}>{errors.sale.message}</div>}
+
     <div>
       <label>Изображение:</label>
-      <input type='file' />
+      <input 
+        {...register('img', {
+          required: 'Обязательно поле!'
+        })}
+        type='file' 
+        name='img'
+      />
     </div>
-    <button>Добавить товар</button>
+
+    {errors.img && <div style={{color: "red"}}>{errors.img.message}</div>}
+
+    <button type='submit'>Добавить товар</button>
+    </form>
   </div>
  )
 }
--- frontend/src/pages/addItem/useAddItem.ts
+++ frontend/src/pages/addItem/useAddItem.ts
@@ -1,8 +1,58 @@
 import { useSelector } from "react-redux"
 import { selectCategories } from "../../redux/category/selectors"
+import { ChangeEvent } from "react"
+import { useMutation } from "react-query"
+import axiosOrigyn from "../../api/axios/axios"
+import { useForm } from "react-hook-form"
+
+type InputFields = {
+  name: string
+  category: string
+  price: string
+  sale?: string
+  img: File
+}
 
 export const useAddItem = () => {
   const categories = useSelector(selectCategories)
 
-  return {categories}
+  const {register, handleSubmit, formState: {errors}} = useForm<InputFields>({
+    mode: 'onChange'
+  })
+
+  const onSubmit = (data: InputFields) => {
+    const resp = uploadImage(data.img)
+    console.log('RESP', resp)
+  }
+
+  const {mutateAsync, isLoading} = useMutation(
+    'uploadFile',
+    (data: FormData) => axiosOrigyn.post('http://localhost:5000/files/upload', data),
+    {
+      onSuccess(data) {
+        console.log('data',data)
+      }
+    }
+  )
+  
+  const uploadImage = async (file: File) => {
+    
+    if(file) {
+      const formData = new FormData()
+      formData.append('image', file)
+      return await mutateAsync(formData)
+    }
+  }
+
+
+  // const uploadImage = async (e: ChangeEvent<HTMLInputElement>) => {
+  //   const files = e.target.files
+  //   if(files?.length) {
+  //     const formData = new FormData()
+  //     formData.append('image', files[0])
+  //     await mutateAsync(formData)
+  //   }
+  // }
+
+  return {categories, uploadImage, isLoading, register, handleSubmit, onSubmit, errors}
 }
