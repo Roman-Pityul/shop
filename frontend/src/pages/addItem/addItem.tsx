705942128839016ba1febff229ece6af6372ba74
Ramon <roman.pityul@gmail.com>
2023-06-23 13:24:28 +0300

    23.06.2023

--- frontend/src/pages/addItem/addItem.tsx
+++ frontend/src/pages/addItem/addItem.tsx
@@ -4,85 +4,96 @@ import style from './addItem.module.scss'
 import { useAddItem } from './useAddItem'
 
 const AddItem: React.FC = () => {
+	const fileInput = React.useRef<HTMLInputElement>(null)
 
-const {categories, uploadImage, register, errors, handleSubmit, onSubmit} = useAddItem()
+	const { categories, uploadImage, register, errors, handleSubmit, onSubmit } =
+		useAddItem()
 
-return (
-  <div className={style.root}>
-    <form onSubmit={handleSubmit(onSubmit)}>
-    <h2>Добавление товара</h2>
+	const handleClick = () => {
+		fileInput.current?.click()
+	}
 
-    <div>
-      <label>Название:</label>
-      <input 
-        {...register('name', {
-          required: 'Обязательное поле!'
-        })}
-        type='text' 
-        name='name'
-        placeholder='Введите название'
-      />
-    </div>
+	return (
+		<div className={style.root}>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<h2>Добавление товара</h2>
 
-    {errors.name && <div style={{color: "red"}}>{errors.name.message}</div>}
+				<div>
+					<label>Название:</label>
+					<input
+						{...register('name', {
+							required: 'Обязательное поле!',
+						})}
+						type="text"
+						name="name"
+						placeholder="Введите название"
+					/>
+				</div>
 
-    <div>
-      <label>Категория:</label>
-      <select
-        {...register('category')}
-        name='category'
-      >
-        {categories && categories.map(cat => (
-          <option>{cat.name}</option>
-        ))}
-      </select>
-    </div>
+				{errors.name && (
+					<div style={{ color: 'red' }}>{errors.name.message}</div>
+				)}
 
-    <div>
-      <label>Цена:</label>
-      <input 
-        {...register('price', {
-          required: 'Обязательное поле!'
-        })}
-        type='text'
-        name='price'
-        placeholder='Введине цену' 
-      />
-    </div>
+				<div>
+					<label>Категория:</label>
+					<select {...register('category')} name="category">
+						{categories && categories.map((cat) => <option>{cat.name}</option>)}
+					</select>
+				</div>
 
-    {errors.price && <div style={{color: "red"}}>{errors.price.message}</div>}
+				<div>
+					<label>Цена:</label>
+					<input
+						{...register('price', {
+							required: 'Обязательное поле!',
+						})}
+						type="text"
+						name="price"
+						placeholder="Введине цену"
+					/>
+				</div>
 
-    <div>
-      <label>Скидка:</label>
-      <input 
-        {...register('sale', {
-          required: 'Обязательное поле!'
-        })}
-        type='text' 
-        name='sale'
-        placeholder='Укажите скидку в %'
-      />
-    </div>
+				{errors.price && (
+					<div style={{ color: 'red' }}>{errors.price.message}</div>
+				)}
 
-    {errors.sale && <div style={{color: "red"}}>{errors.sale.message}</div>}
+				<div>
+					<label>Скидка:</label>
+					<input
+						{...register('sale', {
+							required: 'Обязательное поле!',
+						})}
+						type="text"
+						name="sale"
+						placeholder="Укажите скидку в %"
+					/>
+				</div>
 
-    <div>
-      <label>Изображение:</label>
-      <input 
-        {...register('img', {
-          required: 'Обязательно поле!'
-        })}
-        type='file' 
-        name='img'
-      />
-    </div>
+				{errors.sale && (
+					<div style={{ color: 'red' }}>{errors.sale.message}</div>
+				)}
 
-    {errors.img && <div style={{color: "red"}}>{errors.img.message}</div>}
+				<div>
+					<label>Изображение:</label>
+					<div className={style.uploadButton} onClick={handleClick}>
+						Загрузить файл
+					</div>
+					<input
+						{...register('img', {
+							required: 'Обязательно поле!',
+						})}
+						type="file"
+						name="img"
+						ref={fileInput}
+					/>
+				</div>
 
-    <button type='submit'>Добавить товар</button>
-    </form>
-  </div>
- )
+				{errors.img && <div style={{ color: 'red' }}>{errors.img.message}</div>}
+
+				<button type="submit">Добавить товар</button>
+			</form>
+		</div>
+	)
 }
 
 export default AddItem
