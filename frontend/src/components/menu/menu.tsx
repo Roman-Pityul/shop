c851bdf8098f1eb610a0d663bb74601d0ebc4de7
Ramon <roman.pityul@gmail.com>
2023-07-11 11:05:54 +0300

    11.07.2023

--- frontend/src/components/menu/menu.tsx
+++ frontend/src/components/menu/menu.tsx
@@ -1,47 +1,42 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import cn from 'classnames'
 
-import { setMenuVisible } from "../../redux/head/headReducer"
-import { selectMenuVisible } from "../../redux/head/selectors"
+import { setMenuVisible } from '../../redux/head/headReducer'
+import { selectMenuVisible } from '../../redux/head/selectors'
 
-import "./menu.scss";
+import './menu.scss'
 
 type MenuType = {
-   items: ItemType[]
+	items: ItemType[]
 }
 
 type ItemType = {
-   path: string
-   name: string
+	path: string
+	name: string
 }
 
 const Menu: React.FC<MenuType> = ({ items }) => {
+	const dispatch = useDispatch()
+	const nemuVisible = useSelector(selectMenuVisible)
 
-   const dispatch = useDispatch()
-   const nemuVisible = useSelector(selectMenuVisible)
+	const handleOffVisible = () => {
+		dispatch(setMenuVisible(false))
+	}
 
-   const handleOffVisible = () => {
-      dispatch(setMenuVisible(false))
-   };
+	return (
+		<div className={cn('menu', { menu__active: nemuVisible })}>
+			<div className="menu__container">
+				{items.map((item, index: number) => (
+					<div onClick={handleOffVisible} key={index} className="menu__item">
+						<Link to={`items/${item.path}`}>{item.name}</Link>
+					</div>
+				))}
+			</div>
+			<div onClick={handleOffVisible} className="menu__content"></div>
+		</div>
+	)
+}
 
-   return (
-      <div className={cn('menu', { 'menu__active': nemuVisible })}>
-         <div className="menu__container">
-            {items.map((item, index: number) => (
-               <div
-                  onClick={handleOffVisible}
-                  key={index}
-                  className="menu__item"
-               >
-                  <Link to={item.path}>{item.name}</Link>
-               </div>
-            ))}
-         </div>
-         <div onClick={handleOffVisible} className="menu__content"></div>
-      </div>
-   );
-};
-
-export default Menu;
+export default Menu
